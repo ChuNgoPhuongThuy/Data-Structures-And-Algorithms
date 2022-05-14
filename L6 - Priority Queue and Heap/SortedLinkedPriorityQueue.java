0f6cf173bf4457fe57567077676e2f9038b88ace
Chu Ngô Phương Thủy <91727233+ChuNgoPhuongThuy@users.noreply.github.com>
2022-05-14 18:40:53 +0700

    Update SortedLinkedPriorityQueue.java

--- L6 - Priority Queue and Heap/SortedLinkedPriorityQueue.java
+++ L6 - Priority Queue and Heap/SortedLinkedPriorityQueue.java
@@ -1,4 +1,4 @@
-package hw6_19000299;
+package PriorityQueueAndHeap;
 
 public class SortedLinkedPriorityQueue<K extends Comparable, E> implements PriorityQueueInterface {
     protected static class NodeEntry<K,E> implements Entry<K, E> {
